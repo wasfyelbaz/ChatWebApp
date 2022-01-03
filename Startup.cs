2fa59dd1aec895279e4dadad0aa4dbffda905e7b
wasfyelbaz <wasfylbaz@gmail.com>
2022-01-03 21:30:25 +0200

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChatWebApp.Startup))]
+namespace ChatWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
